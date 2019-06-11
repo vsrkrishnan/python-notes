92b6c61beaf18b368f412a14198ed2cae66af7db
V. Shivaramakrishnan <shivaramakrishnan.viswanathan@gigamon.com>
2019-06-10 21:03:27 -0700

    Singleton code snippet

---
+++ code-snippets/singleton.py
@@ -0,0 +1,9 @@
+# Singleton implementation using new-style classes
+class Singleton(object):
+    def __new__(type):
+        if not '_the_instance' in type.__dict__:
+            type._the_instance = object.__new__(type)
+        return type._the_instance
+
+class Foo(Singleton):
+    pass
